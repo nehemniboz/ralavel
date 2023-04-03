f62e5cbdcea3100122c5699296a92bb62d464a0f
nehemniboz <nehemiaals@gmail.com>
2023-04-03 22:18:47 +0700

    first commit

---
+++ app/Http/Controllers/NehemiaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NehemiaController extends Controller
+{
+    Public function index()
+    {
+        return view('nehemiaweb');
+    }
+};
---
+++ resources/views/mywebsite/nehemiaweb.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>DesigneGrafis</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav>
+    <div class="wrapper">
+        <div class="logo"><a href=''>Design Graphic</a></div>
+        <div class="menu">
+            <ul>
+                <li><a href="#HOME">Home</a></li>
+                <li><a href="TENTANG KAMI.html">Tentang kami</a></li>
+                <li><a href="PRESTASI.html">Prestasi</a></li>
+                <li><a href="CONTAK.html">Contac</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="wrapper">
+    <!----------HOME-->
+    <section id="HOME">
+        <img src="https://3.bp.blogspot.com/-91VEs-6cUr4/WBGKpviCFAI/AAAAAAAAM7k/jsbeLbAfSrkzXCWrJ7vQRkSHPcwiP0tHgCLcB/s1600/3.%2BGraphics-Design.png"/>
+        <div class="kolom">
+            <p class="deskripsi">KAMI DESIGNE GRAFIS BUKAN DESIGNE GRATIS</p>
+            <h2>DESIGNE GRAFIS</h2>
+            <p>Adalah sebuah organisasi atau tempat di mana kalian bisa memesan apapun yang bisa kami designe, dan kalian bebas untuk merancang atau memberi masukan jika apa yang kami buat tidak maksimal, dan lagi untuk revisi itu GRATIS!!!!.</p>
+        </div>
+<div id="Copyright">
+    <div class="wrapper">
+        &copy; 2023 <b>By Nehemiaals</b> All Rights Reserved
+    </div>
+</div>
+</body>
+</html>
